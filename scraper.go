40f7d2a5e75ef76552f5d92a4ff0d061d3d3b65f
tsvtitan <tsv.titan@gmai.com>
2019-12-20 15:12:30 +0200

    Testing github.com

--- scraper.go
+++ scraper.go
@@ -11,7 +11,7 @@ import (
 func main() {
 	// Instantiate default collector
 	c := colly.NewCollector(
-		// MaxDepth is 1, so only the  links on the scraped page
+		// MaxDepth is 1, so only  the  links on the scraped page
 		// is visited, and no further links are followed
 		colly.MaxDepth(-1),
 	)
